ad8a4a61dd88d2f85cae62a16b635ac5ec65dc12
ningning <nwang01@students.uni-mainz.de>
2023-09-20 16:24:40 +0200

    feat: update login, flight_search ect.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, session, redirect
 from flask import render_template
+import re
 
 app = Flask(__name__)
 
@@ -7,11 +8,80 @@ app = Flask(__name__)
 app.secret_key = b"cvobidrnsuerbsifurf34ads"
 
 
+# Statische Flugdaten (für Demonstrationszwecke)
+outward_flights = [
+    {
+        "flight_number": "ABC123",
+        "departure_city": "New York",
+        "destination_city": "Los Angeles",
+        "departure_time": "09:00 AM",
+        "arrival_time": "02:00 PM",
+        "economy_price": 300,
+        "business_price": 600,
+        "first_class_price": 1000,
+    }
+    # Weitere Hinflugdaten
+]
+
+return_flights = [
+    {
+        "flight_number": "XYZ456",
+        "departure_city": "Los Angeles",
+        "destination_city": "New York",
+        "departure_time": "03:00 PM",
+        "arrival_time": "08:00 PM",
+        "economy_price": 300,
+        "business_price": 600,
+        "first_class_price": 1000,
+    }
+    # Weitere Rückflugdaten
+]
+
+
 @app.route("/")
 def home():
     if "username" in session:
-        return render_template("flight_search.html", username=session["username"])
+        return render_template("user-home.html", username=session["username"])
     return render_template("no-session.html")
+
+
+@app.route("/flight_search", methods=["GET"])
+def flight_search():
+    return render_template("flight_search.html")
+
+
+def is_valid_registration_data(firstName, lastName, email, password):
+    # Prüfe, ob Vorname und Nachname nicht leer sind
+    if not firstName or not lastName:
+        return False
+
+    # Prüfe, ob die E-Mail-Adresse ein gültiges E-Mail-Format hat
+    email_pattern = r"^[\w\.-]+@[\w\.-]+\.\w+$"
+    if not re.match(email_pattern, email):
+        return False
+
+    # Prüfe, ob das Passwort bestimmte Anforderungen erfüllt, z. B. Mindestlänge
+    if len(password) < 8:
+        return False
+
+    # Hier kannst du weitere Validierungen hinzufügen, wie z. B. Passwortstärke oder eindeutige E-Mail-Adressen
+
+    # Wenn alle Validierungen erfolgreich sind, gibt die Funktion True zurück
+    return True
+
+
+@app.route("/sign_up", methods=["GET", "POST"])
+def sign_up():
+    if request.method == "POST":
+        firstName = request.form["firstName"]
+        lastName = request.form["lastName"]
+        email = request.form["email"]
+        password = request.form["password"]
+
+        if is_valid_registration_data(firstName, lastName, email, password):
+            return redirect("/flight_search")
+
+    return render_template("sign_up.html")
 
 
 @app.route("/login", methods=["GET", "POST"])
@@ -19,7 +89,7 @@ def login():
     if request.method == "POST":
         # TODO: check if user exists and password is valid
         session["username"] = request.form["username"]
-        return redirect("/")
+        return redirect("/flight_search")
     return render_template("login.html")
 
 
@@ -31,27 +101,26 @@ def logout():
 
 
 @app.route("/search", methods=["POST"])
-def search_flights():
-    departure_date = request.form["departure-date"]
-    return_date = request.form["return-date"]
-
-    # Hier solltest du die Flugdaten aus deiner Datenquelle abrufen.
-    # Dies ist ein Dummy-Beispiel, in dem wir statische Daten verwenden:
-    flights = [
-        {
-            "flight_number": "ABC123",
-            "departure_city": "New York",
-            "destination_city": "Los Angeles",
-            "departure_date": "2023-10-15",
-            "return_date": "2023-10-20",
-        },
-        # Weitere Flugdaten hier
-    ]
-
-    return render_template("search_results.html", flights=flights)
+def search():
+    # Hier könntest du eine Datenbankabfrage oder andere Suchlogik durchführen
+    # Für dieses Beispiel verwenden wir statische Flugdaten
+    return render_template("search_results.html", outward_flights=outward_flights)
 
 
-# ... (deine anderen Routen)
+@app.route("/select-outward-flight", methods=["POST"])
+def select_outward_flight():
+    selected_outward_flight = request.form["selected-outward-flight"]
+    # Hier könntest du die ausgewählten Flugdaten in der Sitzung speichern
+    session["selected_outward_flight"] = selected_outward_flight
+    return render_template("select-return-flight.html", return_flights=return_flights)
+
+
+@app.route("/select-return-flight", methods=["POST"])
+def select_return_flight():
+    selected_return_flight = request.form["selected-return-flight"]
+    session["selected_return_flight"] = selected_return_flight
+    return render_template("booking-summary.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
