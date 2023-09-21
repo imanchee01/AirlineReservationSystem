8ceadae28cfde65c9962e7bdff2f66e9f458bbef
ipeljto <ipeljto@students.uni-mainz.de>
2023-09-21 15:33:45 +0200

    vol2

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from flask import Flask, request, session, redirect
-from flask import render_template
+from flask import Flask, request, session, redirect, render_template
 import re
 
 app = Flask(__name__)
@@ -75,13 +74,37 @@ def sign_up():
     return render_template("sign-up.html")
 
 
-@app.route("/login", methods=["GET", "POST"])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    if request.method == "POST":
-        # TODO: check if user exists and password is valid
-        session["username"] = request.form["username"]
-        return redirect("/flight-search")
-    return render_template("login.html")
+    if request.method == 'POST':
+        username = request.form['user_email']
+        password = request.form['user_password']
+        user_role = get_user_role(username, password)
+
+        if user_type:
+            session['user_type'] = user_type  # Store user_role in session to recognize the user across requests.
+
+            if user_type == 'client':
+                return redirect(
+                    url_for('search_flights'))  # Redirect to the flight search page if the user is a client.
+            elif user_type == 'employee':
+                return redirect(
+                    url_for('manage_requests'))  # Redirect to the manage requests page if the user is an employee.
+        else:
+            flash('Invalid login credentials. Please try again or sign up.', 'error')
+            return redirect(url_for('login'))
+    return render_template('login.html')
+
+@app.route('/search_flights')
+def search_flights():
+    # Your logic for flight search
+    return render_template('search_flights.html')
+
+@app.route('/manage_requests')
+def manage_requests():
+    # Your logic for managing requests
+    return render_template('manage_requests.html')
+
 
 
 @app.route("/logout")
