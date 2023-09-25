f0360bc0d066c9bbfb242cd444c4941578d104d2
paula <paseiber@students.uni-mainz.de>
2023-09-25 14:52:41 +0200

    ...

--- database.py
+++ database.py
@@ -10,13 +10,15 @@ from werkzeug.security import generate_password_hash, check_password_hash
 # Values on the right (or ...) are default values.
 # Values on the left (... or) are values fetched from a .env file.
 # See https://github.com/theskumar/python-dotenv
+
 load_dotenv()
+
 db_config = {
     'user': os.environ.get("DB_ROOTUSER") or 'root',
     'password': os.environ.get("DB_ROOTPASSWORD") or '',
     'host': os.environ.get("DB_HOST") or 'localhost',
     'database': os.environ.get("DB_DATABASE") or 'airline',
-    'port': int(os.environ.get("DB_PORT")) or 3308
+    'port': os.environ.get("DB_PORT") or 3308
 }
 
 app = Flask(__name__)
@@ -68,13 +70,13 @@ def get_user(user_email):
 
 # The get_user_role method is only responsible for returning the user role.
 # It should not be called before the user has been authenticated (not before a password check).
-def get_user_role(user_email):
+def get_user_role(username, password):
     connection = None
     try:
         connection = mariadb.connect(**db_config)
         cursor = connection.cursor(dictionary=True)
-        cursor.execute("SELECT user_type FROM airline.user WHERE user_email = %s",
-                       (user_email, user_password))
+        cursor.execute("SELECT user_type FROM airline.user WHERE user_email = %s AND user_password = %s",
+                       (username, password))
         result = cursor.fetchone()
 
         if result:
@@ -86,6 +88,7 @@ def get_user_role(user_email):
     except mariadb.Error as e:
         print(f"Error connecting to MariaDB Platform: {e}")
         return None
+
     finally:
         if connection:
             connection.close()
@@ -115,9 +118,10 @@ def get_data_for_client(userId):
         cursor = connection.cursor(dictionary=True)
         cursor.execute("""SELECT clientId, miles, tier
                           FROM client
-                          WHERE clientId = %s;""", (userId,))
+                          WHERE clientId = %s""",
+                       (userId, ))
 
-        results = cursor.fetchall()
+        results = cursor.fetchone()
         return results
     except mariadb.Error as e:
         print(f"Error connecting to MariaDB Platform: {e}")
@@ -128,7 +132,6 @@ def get_data_for_client(userId):
 
 
 def get_flights_by_destination(destination):
-    connection = None
     try:
         connection = mariadb.connect(**db_config)
         cursor = connection.cursor(dictionary=True)
@@ -172,8 +175,22 @@ def user_with_email_exists(email):
         return True
     return False
 
+def find_flights(source, destination):
+    connection = None
+    try:
+        connection = mariadb.connect(**db_config)
+        cursor = connection.cursor(dictionary=True)
+        cursor.execute("SELECT * FROM flights WHERE flight_source = %s AND flight_destination = %s",
+                       (source, destination))
+        results = cursor.fetchall()
+        return results
+    except mariadb.Error as e:
+        print(f"Error connecting to MariaDB Platform: {e}")
+        return None
+    finally:
+        if connection:
+            connection.close()
 
 if __name__ == "__main__":
-    userId = 28  # Replace with an actual user_id you want to test.
-    client_data = get_data_for_client(userId)
-    print(client_data)  # This will print the data returned by the function
+    print(get_data_for_client('28'))
+    print(find_flights('FRA', 'ARN'))
